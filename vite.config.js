e43767bc46d4ee4e0f7a8fe6e3878251111f4683
CharanTej <tej3066@gmail.com>
2026-02-25 14:48:06 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// GitHub Pages fix
 export default defineConfig({
-  base: "/FS-BATCH-15/", // 👈 IMPORTANT (repo name)
+  base: '/FS-BATCH-15/',   // ⭐ ADD THIS
   plugins: [react()],
 })
