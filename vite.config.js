9c9db484de690af695fce84a4ffc794ec891038e
CharanTej <tej3066@gmail.com>
2026-02-25 14:35:55 +0530

    Configure base path for GitHub Pages
    Set base path for GitHub Pages deployment.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// GitHub Pages fix
 export default defineConfig({
+  base: "/FS-BATCH-15/", // 👈 IMPORTANT (repo name)
   plugins: [react()],
 })
